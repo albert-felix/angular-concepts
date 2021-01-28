424d4020b7cac9ba2783de353f2b1d18f3a8d896
Albert Felix <albertfelixleo@gmail.com>
2021-01-28 14:20:26 +0530

    one way binding completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'assignment';
   displayName: boolean = true;
-  names: string[];
-  accountNumbers: number[];
+  names: string[] = ["Albert", "Felix", "Kavin", "Kumar"];
+  accountNumbers: number[] = [3216654789, 4776519873, 3577869854, 5874632554];
+  animal: string = 'cat';
 
-  constructor(){
-    this.names = ["Albert", "Felix", "Kavin", "Kumar"]
-    this.accountNumbers = [3216654789, 4776519873, 3577869854, 5874632554]
-  }
+  constructor(){  }
 
   toggleDisplay(){
     this.displayName = !this.displayName;
+  }
+
+  changeImage(e: Event){
+    const animal = e.target as HTMLInputElement
+    this.animal = animal.value.toLowerCase()
   }
 }
