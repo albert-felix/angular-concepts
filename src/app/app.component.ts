6cb988df1b0139c520d7b1df586ca28e1a622e49
Albert Felix <albertfelixleo@gmail.com>
2021-01-28 12:24:20 +0530

    directives ngIf and ngFor completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment';
+  displayName: boolean = true;
+  names: string[];
+  accountNumbers: number[];
+
+  constructor(){
+    this.names = ["Albert", "Felix", "Kavin", "Kumar"]
+    this.accountNumbers = [3216654789, 4776519873, 3577869854, 5874632554]
+  }
+
+  toggleDisplay(){
+    this.displayName = !this.displayName;
+  }
 }
